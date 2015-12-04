e6fea8fbc381a7f0ef8922aeaa884a295536e0c4
Demond Simmons <bamtype@gmail.com>
2015-12-03 20:08:19 -0500

    last commit before heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 	class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
-	# before_action :authenticate_user!, except: [:index, :show]
+	 
 
 	def index
 		@posts = Post.all.order("created_at DESC")
@@ -52,7 +52,7 @@
 		redirect_to :back
 	end
 
-	private
+private
 
 	def find_post
 		@post = Post.find(params[:id])
